ca5f4b5514af4f2cb84979afac54883025220e91
Ashley Knapp <ajknapp@ucsc.edu>
2024-11-06 14:41:36 -0800

    L16 update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const testBtn = document.createElement("button");
 testBtn.innerHTML = "Test";
-app.append(testBtn)
+app.append(testBtn);
 
 testBtn.addEventListener("click", function () {
     alert("Button Clicked");
