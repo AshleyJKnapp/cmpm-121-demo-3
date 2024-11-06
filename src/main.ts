349a4417eb7555c6ca8c0075d6295d1e63fe29be
Ashley Knapp <ajknapp@ucsc.edu>
2024-11-06 14:19:47 -0800

    L16 initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const testBtn = document.createElement("button");
+testBtn.innerHTML = "Test";
+app.append(testBtn)
+
+testBtn.addEventListener("click", function () {
+    alert("Button Clicked");
+});
