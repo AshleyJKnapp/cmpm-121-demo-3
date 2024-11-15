8adb7e2d1dde200c6ad0173646fbd816628d1946
Ashley Knapp <ajknapp@ucsc.edu>
2024-11-15 07:55:45 -0800

    D3.b Flyweight and global coordinate done

---
+++ src/board.ts
@@ -0,0 +1,71 @@
+import leaflet from "leaflet";
+
+interface Cell {
+  readonly i: number;
+  readonly j: number;
+}
+
+export class Board {
+  readonly tileWidth: number;
+  readonly tileVisibilityRadius: number;
+  private readonly knownCells: Map<string, Cell>;
+
+  constructor(tileWidth: number, tileVisibilityRadius: number) {
+    this.tileWidth = tileWidth;
+    this.tileVisibilityRadius = tileVisibilityRadius;
+    this.knownCells = new Map<string, Cell>();
+  }
+
+  private getCanonicalCell(cell: Cell): Cell {
+    const { i, j } = cell;
+    const key = [i, j].toString();
+
+    // Add Cell if it does not exist
+    if (!this.knownCells.has(key)) {
+      this.knownCells.set(key, cell)!;
+    }
+    return this.knownCells.get(key)!;
+  }
+
+  getCellForPoint(point: leaflet.LatLng): Cell {
+    return this.getCanonicalCell({
+      i: Math.floor(point.lat * 1e4),
+      j: Math.floor(point.lng * 1e4),
+    });
+  }
+
+  getCellBounds(cell: Cell): leaflet.LatLngBounds {
+    const bounds = leaflet.latLngBounds([
+      [
+        cell.i / 1e4,
+        cell.j / 1e4,
+      ],
+      [
+        (cell.i + 1) / 1e4,
+        (cell.j + 1) / 1e4,
+      ],
+    ]);
+    return leaflet.latLngBounds(bounds);
+  }
+
+  getCellsNearPoint(point: leaflet.LatLng): Cell[] {
+    const resultCells: Cell[] = [];
+    const originCell = this.getCellForPoint(point);
+    for (
+      let c = -this.tileVisibilityRadius;
+      c <= this.tileVisibilityRadius;
+      c++
+    ) {
+      for (
+        let r = -this.tileVisibilityRadius;
+        r <= this.tileVisibilityRadius;
+        r++
+      ) {
+        resultCells.push(
+          this.getCanonicalCell({ i: originCell.i + c, j: originCell.j + r }),
+        );
+      }
+    }
+    return resultCells;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // @deno-types="npm:@types/leaflet@^1.9.14"
 import leaflet from "leaflet";
+import { Board } from "./board.ts";
 
 // Style sheets
 import "leaflet/dist/leaflet.css";
@@ -10,12 +11,15 @@ import "./leafletWorkaround.ts";
 
 // Deterministic random number generator
 import luck from "./luck.ts";
+// const temp = new Board(5, 10);
 
 // Location of our classroom (as identified on Google Maps)
 const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
 
 // Tunable gameplay parameters
 const GAMEPLAY_ZOOM_LEVEL = 19;
+const GAMEPLAY_MIN_ZOOM_LEVEL = 18;
+// const GAMEPLAY_MAX_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const CACHE_SPAWN_PROBABILITY = 0.1;
@@ -24,11 +28,11 @@ const CACHE_SPAWN_PROBABILITY = 0.1;
 const map = leaflet.map(document.getElementById("map")!, {
   center: OAKES_CLASSROOM,
   zoom: GAMEPLAY_ZOOM_LEVEL,
-  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_MIN_ZOOM_LEVEL,
   maxZoom: GAMEPLAY_ZOOM_LEVEL,
   zoomControl: false,
-  scrollWheelZoom: false,
-  dragging: false,
+  scrollWheelZoom: true,
+  dragging: true,
 });
 
 // Populate the map with a background tile layer
@@ -46,56 +50,66 @@ playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
 
 interface Cell {
-  i: number;
-  j: number;
+  readonly i: number;
+  readonly j: number;
 }
 
 interface Coin {
   cell: Cell;
+  serial: number;
 }
 
 interface Cache {
   coins: Coin[];
+  cell: Cell;
 }
 
-// Transfers an amount from the sender cache to reciever cache
-// Returns true if successful, false otherwise
-function transferCoins(sender: Cache, reciever: Cache, amt: number) {
-  if (sender.coins.length >= amt) {
-    const tempCoin = sender.coins.pop()!;
-    reciever.coins.push(tempCoin);
-    return true;
+// Transfers an amount from the sender Coin[] to reciever Coin[]
+// Returns the transferred coin if successful, null otherwise
+function transferCoins(sender: Coin[], reciever: Coin[], amt: number) {
+  if (sender.length >= amt) {
+    const tempCoin = sender.pop()!;
+    reciever.push(tempCoin);
+    return tempCoin;
   } else {
-    return false;
+    return null;
   }
 }
 
 // Display the player's points
-const playerCache: Cache = { coins: [] };
-// let playerPoints = 0;
+const playerCoins: Coin[] = [];
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
 statusPanel.innerHTML = "No points yet...";
+
+// TODO: Implement player-inventory-changed
+// Show all of the tokens the player owns
+
+const mapBoard = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
 
 // Add caches to the map by cell numbers
 function spawnCache(i: number, j: number) {
   // Convert cell numbers into lat/lng bounds
   const origin = OAKES_CLASSROOM;
-  const bounds = leaflet.latLngBounds([
-    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
-    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
-  ]);
+  i = origin.lat + i * TILE_DEGREES;
+  j = origin.lng + j * TILE_DEGREES;
+
+  // Set Cache location with flyweight
+  const point: leaflet.LatLng = leaflet.latLng(i, j);
+  const thisCell = mapBoard.getCellForPoint(point);
+  const bounds = mapBoard.getCellBounds(thisCell);
 
   // Add a rectangle to the map to represent the cache
   const rect = leaflet.rectangle(bounds);
   rect.addTo(map);
-  const cache: Cache = { coins: [] };
+  const cache: Cache = { coins: [], cell: thisCell };
 
   // Each cache has a random point value, mutable by the player
   const pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
 
-  for (let i = 0; i < pointValue; i++) {
-    const cell: Cell = { i, j };
-    const coin: Coin = { cell };
+  for (let k = 0; k < pointValue; k++) {
+    const cell: Cell = thisCell;
+    const serial: number = k;
+    const coin: Coin = { cell, serial };
     cache.coins.push(coin);
   }
 
@@ -104,7 +118,7 @@ function spawnCache(i: number, j: number) {
     // The popup offers a description and button
     const popupDiv = document.createElement("div");
     popupDiv.innerHTML = `
-                <div>There is a cache here at "${i},${j}". It has value <span id="value">${cache.coins.length}</span>.</div>
+                <div>There is a cache here at "${thisCell.i},${thisCell.j}". It has <span id="value">${cache.coins.length}</span> coins</span>.</div>
                 <button id="collect">collect</button>
                 <button id="deposit">deposit</button>`;
 
@@ -112,12 +126,13 @@ function spawnCache(i: number, j: number) {
     popupDiv
       .querySelector<HTMLButtonElement>("#collect")!
       .addEventListener("click", () => {
-        // If transferring 1 from cache to playerCache succeeds
-        if (transferCoins(cache, playerCache, 1)) {
+        const transferred = transferCoins(cache.coins, playerCoins, 1);
+        // If transferring 1 from cache to playerCoins succeeds
+        if (transferred) {
           popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = cache
             .coins.length.toString();
           statusPanel.innerHTML =
-            `${playerCache.coins.length} points accumulated`;
+            `Collected Coin ${transferred.cell.i}:${transferred.cell.j}#${transferred.serial}<br>${playerCoins.length} points accumulated`;
         } else {
           alert("you sucked it dry bruh");
         }
@@ -127,12 +142,13 @@ function spawnCache(i: number, j: number) {
     popupDiv
       .querySelector<HTMLButtonElement>("#deposit")!
       .addEventListener("click", () => {
+        const transferred = transferCoins(playerCoins, cache.coins, 1);
         // If transferring 1 from playerCache to cache succeeds
-        if (transferCoins(playerCache, cache, 1)) {
+        if (transferred) {
           popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = cache
             .coins.length.toString();
           statusPanel.innerHTML =
-            `${playerCache.coins.length} points accumulated`;
+            `Deposited Coin ${transferred.cell.i}:${transferred.cell.j}#${transferred.serial}<br>${playerCoins.length} points accumulated`;
         } else {
           alert("you are too broke bruh");
         }
